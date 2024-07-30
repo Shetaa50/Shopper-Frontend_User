ba7eaffb8503c10fa28b906cfe0d10b9ca2d8b98
Shetaa50 <122708123+Shetaa50@users.noreply.github.com>
2024-07-30 20:42:02 +0300

    Update Product.jsx

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -5,8 +5,8 @@ import { ShopContext } from "../context/ShopContext";
 import { useParams } from "react-router-dom";
 import Piece from "../components/piece/Piece";
 import Display  from "../components/Display/Display";
-import DescriptionBox from "../components/dicsriptionbox/DescriptionBox";
-import RelatedSection from "../components/RelatedSection/RelatedSection";
+// import DescriptionBox from "../components/dicsriptionbox/DescriptionBox";
+// import RelatedSection from "../components/RelatedSection/RelatedSection";
 const Product = ()=>{
     const{all_product} = useContext(ShopContext)
     const{productId} =useParams();
